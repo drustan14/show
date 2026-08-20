ab56e56b678ae1b1598555d56a0d7e1e00f5a2c8
drustan <drustan.14@outlook.com>
2026-08-20 13:44:33 +0800

    init flutter web gallery

--- a.bat
+++ a.bat
@@ -2,5 +2,5 @@ git init
 git add .
 git commit -m "init flutter web gallery"
 git branch -M main
-git remote add origin https://github.com/你的用户名/flutter-web-gallery.git
+git remote add origin https://github.com/drustan14/show.git
 git push -u origin main
